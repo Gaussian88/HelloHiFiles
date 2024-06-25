79260146d2b7ed1799021a0fd88afa6f8ca56e5a
Gaussian8888 <gaussian8888@gmail.com>
2024-06-25 09:16:03 +0900

    06-25-에 제작한 파일
    제작한 내용에 대해서 상세한 내용을 적는다.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//using Major;
+//namespace _06_24_NetCore
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//           MathMatic math = new MathMatic();
+//            Console.WriteLine(math.Max(1,2));
+//            Console.WriteLine(math.Min(4, 3));
+//        }
+//    }
+//}
